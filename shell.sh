977aa7f2e5e8e1dbf18555dbd7b9fa2229a04c2b
alan <783242528@qq.com>
2015-06-10 21:45:17 +0800

    add a shell.sh

---
+++ shell.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#/etc/passwd
+#base command: pwd ls find more gzip who mkdir rmdir touch rm cat grep cp mv 
+#chmod sh ./ 
+#-e error exit; -n read no execture; -u chech var; -v read input line; -x execture and show
+
+AA="HELLO WORD"
+echo $AA
+test -n "$AA"
+test -z "$AA"
+
+# -eq -ge -ne -lt -le
+#! -a -o
+
+if [ ] ;then
+elif [ ]
+fi
+case i in 
+1)
+;;
+2)
+;;
+*)
+esac
+
+for i in [ ]
+do
+done
+
+while
+do
+done
+
+until
+do
+done
+
+
+
+
+
+
+
